38bd5a5ebf245c6e63f030f2fbb33c632fc3e873
Evelina <evelina.evish@yandex.ru>
2022-05-18 20:41:04 +0300

    Задача 10 работает

--- 10_Задача/Program.cs
+++ 10_Задача/Program.cs
@@ -4,4 +4,21 @@
 // 197 -> 9
 
 
+Console.WriteLine("Введите трехзначное число");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number > 999)
+{
+    Console.WriteLine("Я зря написала ТРЕХЗНАЧНОЕ число? Число слишком длинное");
+    
+}
+else if (number < 100)
+{
+    Console.WriteLine("Я зря написала ТРЕХЗНАЧНОЕ число? Число недостаточно длинное");
+}
+else
+{
+    int number2 = number / 10 % 10;
+    Console.WriteLine("Вторая цифра " + number + " = " + number2);
+}
 
